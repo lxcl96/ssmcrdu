f288052e12b3eaaaa8100ff6cb569379c648df20
ly <lxcl96@vip.qq.com>
2022-06-19 21:33:21 +0800

    列表展示

--- src/main/java/com/ly/ssm/controller/EmployeeController.java
+++ src/main/java/com/ly/ssm/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,20 +18,20 @@ import org.springframework.web.servlet.ModelAndView;
  * @Description: 处理员工的CRUD请求
  */
 @Controller
-@RequestMapping("/emps")
+@RequestMapping({"/emps"})
 public class EmployeeController {
     private Logger logger = LoggerFactory.getLogger(EmployeeController.class);
     @Autowired
     private EmployeeService employeeService;
     /**
-     * get请求 返回所有员工列表 (分页查询)
+     * get请求 返回所有员工列表 (分页查询) 【三种请求地址】
      * @param pageNo 当前页码  （sql中为pageNo -1）
      * @param pageSize 每页显示几条数据，默认是5跳
      * @return 跳转到员工列表展示页面
      */
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView getEmpList(@RequestParam(required = false,value = "pageNo") Integer pageNo,
-                             @RequestParam(required = false,value = "pageSize") Integer pageSize,ModelAndView modelAndView) {
+    @RequestMapping(method = RequestMethod.GET,value = {"/{pageNo}/{pageSize}","/{pageNo}",""})
+    public ModelAndView getEmpList(@PathVariable(required = false,value = "pageNo") Integer pageNo,
+                             @PathVariable(required = false,value = "pageSize") Integer pageSize,ModelAndView modelAndView) {
         if (pageNo == null) {
             if (logger.isDebugEnabled()) {
                 logger.debug("当前请求没有携带当前页码 参数：pageNo，默认为 1");
@@ -50,4 +51,5 @@ public class EmployeeController {
 
         return modelAndView;
     }
+
 }
